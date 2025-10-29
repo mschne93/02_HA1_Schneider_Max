7852b65520ae2dda6845ab00f6d77d7f6f844e4e
nichtmax06 <122627896+nichtmax06@users.noreply.github.com>
2025-10-29 13:46:01 +0100

    v1

---
+++ src/h2/H2_main.java
@@ -0,0 +1,16 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args) {
+        int i, j, k, min, max;
+        i =1;
+        j = 7;
+        k = 9;
+
+        min = Math.min(j, Math.min(k,i));
+
+        System.out.printf("min)
+
+    }
+
+}
