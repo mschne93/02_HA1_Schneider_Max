16a595fcdff4426fe315a23475579d131e9f8c01
nichtmax06 <122627896+nichtmax06@users.noreply.github.com>
2025-10-29 14:46:36 +0100

    v

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,14 +3,30 @@ package h2;
 public class H2_main {
     public static void main(String[] args) {
         int i, j, k, min, max;
-        i =9;
+        i = 9;
         j = 7;
         k = 2;
 
-        min = Math.min(j, Math.min(k,i));
+        // Min
+        if(i < j && i < k) {
+            min = i;
+        } else if(j < k) {
+            min = j;
+        } else {
+            min = k;
+        }
+
+        //Max
+        if(i > j && i > k) {
+            max = i;
+        } else if(j > k) {
+            max = j;
+        } else {
+            max = k;
+        }
 
         System.out.println(min);
-
+        System.out.println(max);
     }
 
 }
--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -3,7 +3,27 @@ package h3;
 public class H3_main {
     public static void main(String[] args) {
         int i, j, k;
+        boolean f1, f2, f3;
+        i = 500;
+        j = 200;
+        k = -10;
 
+        f1 = i > j;
+        f2 = i > 200;
+        f3 = j > 100;
 
+        if(f1 && !f2 && !f3) {
+            k = 1;
+        } else if(f1 && f2 && !f3){
+            k = 2;
+        } else if(f1 && f2 && f3) {
+            k = 3;
+        } else if(!f1 && !f2 && !f3){
+            k = 4;
+        } else {
+            k = -10;
+        }
+
+        System.out.println(k);
     }
 }
