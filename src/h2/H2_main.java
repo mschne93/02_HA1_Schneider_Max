c625d33ea1ae4329f28e2cbfc44dd9897a73f757
nichtmax06 <122627896+nichtmax06@users.noreply.github.com>
2025-10-29 13:56:17 +0100

    v1

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,13 +3,13 @@ package h2;
 public class H2_main {
     public static void main(String[] args) {
         int i, j, k, min, max;
-        i =1;
+        i =9;
         j = 7;
-        k = 9;
+        k = 2;
 
         min = Math.min(j, Math.min(k,i));
 
-        System.out.printf("min)
+        System.out.println(min);
 
     }
 
---
+++ src/h3/H3_main.java
@@ -0,0 +1,9 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i, j, k;
+
+
+    }
+}
